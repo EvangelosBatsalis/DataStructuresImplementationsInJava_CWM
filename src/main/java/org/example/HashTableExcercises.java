82ad13a58d52c3433d4aa15f2983fddaf499e2b6
Evangelos Batsalis <v.batsalis@gmail.com>
2023-07-10 22:05:49 +0300

    added binary tree implementations (insert and find methof)

--- src/main/java/org/example/HashTableExcercises.java
+++ src/main/java/org/example/HashTableExcercises.java
@@ -5,22 +5,17 @@ import java.util.Map;
 
 public class HashTableExcercises {
 
-    public int mostFrequent(int[] array){
+    public int mostFrequent(int[] array) {
         int count = 0;
         Map<Integer, Integer> map = new HashMap<>();
-        for(int items : array){
-            if(map.containsKey(items)){
+        for (int items : array) {
+            if (map.containsKey(items)) {
                 map.put(items, ++count);
             }
             map.put(items, count);
         }
-
-        int mostFrequentItems = 0;
-        for(var items : map.keySet()){
-            if(items.floatValue() > mostFrequentItems){
-                mostFrequentItems = ma
-            }
-        }
+        return 0;
+    }
 
 
 
---
+++ src/main/java/org/example/Tree.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Tree {
+    private class Node{
+        int value;
+        Node lefChild;
+        Node rightChild;
+    }
+
+    Node root;
+
+    public Tree(int value){
+        root.value = value;
+    }
+
+    // TODO: 31/5/2023 insert(value)
+    // TODO: 31/5/2023 find(value): boolean
+
+
+
+
+
+}
